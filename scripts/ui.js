c9f70d635163ed056ccf2989993b55b1132f92d6
Kacper <kacper.ksiez@gmail.com>
2022-10-10 21:27:51 +0000

    mobile tap highlight color delete

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,15 +1,19 @@
-class UIController{
-    constructor(){
-        this.mainHeaderNavBtn = document.querySelector('.main-header__navBtn')
-        this.mainHeaderNav = document.querySelector('.main-header__nav')
-    }
-    mainHeaderMenuSwitch(){
-        document.querySelector('.main-header__navBtn').addEventListener('click', ()=>{
-            this.mainHeaderNav.classList.toggle('main-header__nav--hidden')
-            this.mainHeaderNavBtn.classList.toggle('main-header__navBtn--clicked')
-        })
-    }
+class UIController {
+	constructor() {
+		this.mainHeaderNavBtn = document.querySelector(".main-header__navBtn");
+		this.mainHeaderNav = document.querySelector(".main-header__nav");
+	}
+	mainHeaderMenuSwitch() {
+		document
+			.querySelector(".main-header__navBtn")
+			.addEventListener("click", () => {
+				this.mainHeaderNav.classList.toggle("main-header__nav--hidden");
+				this.mainHeaderNavBtn.classList.toggle(
+					"main-header__navBtn--clicked"
+				);
+			});
+	}
 }
 
-let uictrl = new UIController()
-uictrl.mainHeaderMenuSwitch()
+let uictrl = new UIController();
+uictrl.mainHeaderMenuSwitch();
