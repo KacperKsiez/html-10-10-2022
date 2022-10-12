473f23000e8fd692ff7a21ac1b6964c6971b9469
Kacper <kacper.ksiez@gmail.com>
2022-10-12 22:46:18 +0000

    preload switcher i intersection observer authors

--- scripts/preloader.js
+++ scripts/preloader.js
@@ -1,22 +1,25 @@
-// (() => {
-// 	document.body.style.overflow = 'hidden';
-// 	window.onload = () => {
-// 		const preloader = document.querySelector('.preloader');
-// 		const preloaderCircle = document.querySelector('.preloader__circle');
+((on) => {
+	const preloader = document.querySelector('.preloader');
+	const preloaderCircle = document.querySelector('.preloader__circle');
+	if (on) {
+		window.onload = () => {
+			preloaderCircle.classList.add('preloader__circle--fadeOut');
+			preloaderCircle.classList.remove('preloader__circle--rotate');
+			document.body.style.overflow = 'hidden';
 
-// 		preloaderCircle.classList.add('preloader__circle--fadeOut');
-// 		preloaderCircle.classList.remove('preloader__circle--rotate');
-
-// 		setTimeout(() => {
-// 			preloader.classList.add('preloader--transform');
-// 		}, 450);
-// 		setTimeout(() => {
-// 			preloader.remove();
-// 		}, 1450);
-// 		setTimeout(() => {
-// 			const fullsiteHeader = document.querySelector('.fullsite__text');
-// 			fullsiteHeader.classList.toggle('fullsite__text--lighting');
-// 			document.body.style.overflowY = 'scroll';
-// 		}, 600);
-// 	};
-// })();
+			setTimeout(() => {
+				preloader.classList.add('preloader--transform');
+			}, 450);
+			setTimeout(() => {
+				preloader.remove();
+			}, 1450);
+			setTimeout(() => {
+				const fullsiteHeader = document.querySelector('.fullsite__text');
+				fullsiteHeader.classList.toggle('fullsite__text--lighting');
+				document.body.style.overflowY = 'scroll';
+			}, 600);
+		};
+	} else {
+		preloader.remove();
+	}
+})(false);
--- scripts/ui.js
+++ scripts/ui.js
@@ -4,6 +4,7 @@ class UIController {
 		this.mainHeaderNav = document.querySelector('.main-header__nav');
 		this.fullsite = document.querySelector('.fullsite');
 		this.header = document.querySelector('.main-header');
+		this.authors = document.querySelectorAll('.content__author');
 	}
 	mainHeaderMenuSwitch() {
 		document.querySelector('.main-header__navBtn').addEventListener('click', () => {
@@ -11,7 +12,29 @@ class UIController {
 			this.mainHeaderNavBtn.classList.toggle('main-header__navBtn--clicked');
 		});
 	}
+	authorsIntersectionObserver() {
+		const obs = new IntersectionObserver(
+			(authors) => {
+				const images = document.querySelectorAll('.content__author-image');
+				authors.forEach((author) => {
+					if (author.isIntersecting) {
+						images.forEach((image, num) => {
+							image.classList.add('content__author-image--fadeIn');
+							image.classList.add(`delay${num}`);
+						});
+					}
+				});
+			},
+			{
+				rootMargin: '-120px',
+			}
+		);
+		this.authors.forEach((author) => {
+			obs.observe(author);
+		});
+	}
 }
 
 let uictrl = new UIController();
 uictrl.mainHeaderMenuSwitch();
+uictrl.authorsIntersectionObserver();
