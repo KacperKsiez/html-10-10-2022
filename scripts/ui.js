4bc26019fad6999493df8c098c97977278a2ba23
Kacper <kacper.ksiez@gmail.com>
2022-10-12 18:18:54 +0000

    merge fork

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,19 +1,20 @@
 class UIController {
 	constructor() {
-		this.mainHeaderNavBtn = document.querySelector(".main-header__navBtn");
-		this.mainHeaderNav = document.querySelector(".main-header__nav");
+		this.mainHeaderNavBtn = document.querySelector('.main-header__navBtn');
+		this.mainHeaderNav = document.querySelector('.main-header__nav');
 	}
 	mainHeaderMenuSwitch() {
-		document
-			.querySelector(".main-header__navBtn")
-			.addEventListener("click", () => {
-				this.mainHeaderNav.classList.toggle("main-header__nav--hidden");
-				this.mainHeaderNavBtn.classList.toggle(
-					"main-header__navBtn--clicked"
-				);
-			});
+		document.querySelector('.main-header__navBtn').addEventListener('click', () => {
+			this.mainHeaderNav.classList.toggle('main-header__nav--hidden');
+			this.mainHeaderNavBtn.classList.toggle('main-header__navBtn--clicked');
+		});
 	}
 }
 
 let uictrl = new UIController();
 uictrl.mainHeaderMenuSwitch();
+
+let a = {
+	a: 'b',
+	b: '33',
+};
